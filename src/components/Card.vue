c2b32f4ccf74fd83cde19c18466c0ab100f26fa5
canberkekinci <1canberkekinci@gmail.com>
2025-09-16 00:26:02 +0300

    Initial Commit

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  cssClass: {
+    type: String,
+    default: "bg-gray-100 p-6 rounded-lg shadow-m",
+  },
+});
+</script>
+
+<template>
+  <div :class="cssClass">
+    <slot></slot>
+  </div>
+</template>
